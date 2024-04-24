1cad9c66d99270a98d98b93adf17001fba33c2bb
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-04-24 16:57:49 +0300

    GenericRepository added

---
+++ SignalR.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Repositories
+{
+    internal class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly SignalRContext _context;
+
+        public GenericRepository(SignalRContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
