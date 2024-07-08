7be315656ce45938e6dc99c93bfd51542d83ee91
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-07-08 17:27:10 +0300

    HeaderLayout added

--- SignalRApi/obj/Debug/net6.0/SignalRApi.AssemblyInfo.cs
+++ SignalRApi/obj/Debug/net6.0/SignalRApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372ca61015a6ae3ec66c96e585460c8f0be5ec4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
