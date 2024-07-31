312f113acd7c898af3b2bde5ac8fbb085d9fc799
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-07-31 17:55:27 +0300

    Product added

--- SignalR.DtoLayer/obj/Debug/net6.0/SignalR.DtoLayer.AssemblyInfo.cs
+++ SignalR.DtoLayer/obj/Debug/net6.0/SignalR.DtoLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR.DtoLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29a7d850022ea9b368f153b3439dd0f544504e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR.DtoLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR.DtoLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
